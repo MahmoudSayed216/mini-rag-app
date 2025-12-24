e3d10fac31a753361ee4dc6ae4658760dbb95380
MahmoudSayed216 <mahmoud_das@outlook.com>
2025-12-25 00:05:31 +0200

    started building the routers and invoked the .env file

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
+from routes import base
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 
 app = FastAPI()
-
-@app.get("/welcome")
-def welcome():
-    return {"message" : "Welcome to summoner's rift"}
-
+app.include_router(base.base_router)
---
+++ routes/base.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, APIRouter
+import os
+
+
+
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"]
+    )
+
+
+
+@base_router.get("/")
+def welcome():
+    app_name = os.getenv('APP_NAME')
+    app_version = os.getenv('VERSION')
+
+    return {"message" : f"{app_name} v{app_version}"}
+
